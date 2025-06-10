2b6bc169c88ade2d327384ca550785826e27b44a
Abu Sayeed Rifat <abusayeedrifat20@gmail.com>
2025-06-10 06:29:14 +0600

    3rd commit

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 const dropdownItems = document.querySelectorAll(".dropdown-hover");
 
 dropdownItems.forEach((dropdownItem) => {
-  dropdownItem.addEventListener("mouseover", () => {
+  let timeoutId;
+  dropdownItem.addEventListener("mouseenter", () => {
+    clearTimeout(timeoutId);
+
+    document.querySelectorAll(".nav-dropdown").forEach((dropdown) => {
+      dropdown.classList.remove("show");
+    });
+
     const dropdown = dropdownItem.lastElementChild;
     dropdown.classList.add("show");
 
@@ -12,13 +19,19 @@ dropdownItems.forEach((dropdownItem) => {
     navbarWrapper.classList.add("dropdown-active");
   });
 
-  dropdownItem.addEventListener("mouseout", () => {
-    const dropdown = dropdownItem.lastElementChild;
-    dropdown.classList.remove("show");
+  dropdownItem.addEventListener("mouseleave", () => {
+    timeoutId = setTimeout(() => {
+      const anyDropdownActive = document.querySelector(".dropdown-hover:hover");
+      if (!anyDropdownActive) {
+        document.querySelectorAll(".nav-dropdown").forEach((dropdown) => {
+          dropdown.classList.remove("show");
+        });
 
-    const navbarWrapper = document.querySelector(".navbar-wrapper");
-    navbarWrapper.style.background = "none";
-    navbarWrapper.classList.remove("dropdown-active");
+        const navbarWrapper = document.querySelector(".navbar-wrapper");
+        navbarWrapper.style.background = "none";
+        navbarWrapper.classList.remove("dropdown-active");
+      }
+    }, 100);
   });
 });
 // End of Navigation
